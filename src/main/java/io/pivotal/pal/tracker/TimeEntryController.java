7311f4173c6eb21340e3ba7f66591e15f5a53021
vennilagp <vennilagp@gmail.com>
2020-04-05 18:39:33 +0530

    Actuator

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -12,37 +12,49 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
+
 @RestController
 public class TimeEntryController {
 
 	private TimeEntryRepository timeEntryRepository;
+	private final DistributionSummary timeEntrySummary;
+	private final Counter actionCounter;
 
-	public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+	public TimeEntryController(TimeEntryRepository timeEntryRepository, MeterRegistry meterRegistry) {
+		
 		this.timeEntryRepository=timeEntryRepository;
+		timeEntrySummary = meterRegistry.summary("timeEntry.summary");
+        actionCounter = meterRegistry.counter("timeEntry.actionCounter");
 	}
 
 	@PostMapping("/time-entries")
 	public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntryToCreate) {
 		TimeEntry createResponse = timeEntryRepository.create(timeEntryToCreate);
-
+		actionCounter.increment();
+        timeEntrySummary.record(timeEntryRepository.list().size());
 		return new ResponseEntity<TimeEntry>(createResponse,HttpStatus.CREATED);
 	}
-
+	
 	@GetMapping("/time-entries/{id}")
 	public ResponseEntity<TimeEntry> read(@PathVariable("id") long timeEntryId) {
 		TimeEntry readResponse = timeEntryRepository.find(timeEntryId);
 		if(readResponse!=null) {
+			 actionCounter.increment();
 			return new ResponseEntity<TimeEntry>(readResponse,HttpStatus.OK);
 		}else {
 			return new ResponseEntity<TimeEntry>(HttpStatus.NOT_FOUND);
 		}
-
-
+		
+		
 	}
-
+	
 	@GetMapping("/time-entries")
 	public ResponseEntity<List<TimeEntry>> list() {
 		List<TimeEntry> listcreateResponse = timeEntryRepository.list();
+		actionCounter.increment();
 		return new ResponseEntity<List<TimeEntry>>(listcreateResponse,HttpStatus.OK);
 	}
 
@@ -50,6 +62,7 @@ public class TimeEntryController {
 	public ResponseEntity<TimeEntry> update(@PathVariable("id") long timeEntryId, @RequestBody TimeEntry expected) {
 		TimeEntry updateResponse = timeEntryRepository.update(timeEntryId, expected);
 		if(updateResponse!=null) {
+			actionCounter.increment();
 			return new ResponseEntity<TimeEntry>(updateResponse,HttpStatus.OK);
 		}else {
 			return new ResponseEntity<TimeEntry>(HttpStatus.NOT_FOUND);
@@ -59,6 +72,9 @@ public class TimeEntryController {
 	@DeleteMapping("/time-entries/{id}")
 	public ResponseEntity<TimeEntry> delete(@PathVariable("id") long timeEntryId) {
 		timeEntryRepository.delete(timeEntryId);
+		actionCounter.increment();
+        timeEntrySummary.record(timeEntryRepository.list().size());
 		return new ResponseEntity<TimeEntry>(HttpStatus.NO_CONTENT);
 	}
+
 }
--- src/test/java/test/pivotal/pal/tracker/TimeEntryControllerTest.java
+++ src/test/java/test/pivotal/pal/tracker/TimeEntryControllerTest.java
@@ -1,5 +1,8 @@
 package test.pivotal.pal.tracker;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.DistributionSummary;
+import io.micrometer.core.instrument.MeterRegistry;
 import io.pivotal.pal.tracker.TimeEntry;
 import io.pivotal.pal.tracker.TimeEntryController;
 import io.pivotal.pal.tracker.TimeEntryRepository;
@@ -23,8 +26,18 @@ public class TimeEntryControllerTest {
 
     @BeforeEach
     public void setUp() {
-        timeEntryRepository = mock(TimeEntryRepository.class);
-        controller = new TimeEntryController(timeEntryRepository);
+    	 timeEntryRepository = mock(TimeEntryRepository.class);
+    	    MeterRegistry meterRegistry = mock(MeterRegistry.class);
+
+    	    doReturn(mock(DistributionSummary.class))
+    	            .when(meterRegistry)
+    	            .summary("timeEntry.summary");
+
+    	    doReturn(mock(Counter.class))
+    	            .when(meterRegistry)
+    	            .counter("timeEntry.actionCounter");
+
+    	    controller = new TimeEntryController(timeEntryRepository, meterRegistry);
     }
 
     @Test
